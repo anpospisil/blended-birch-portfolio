f4e5747c9612a368abb0b5543a31b1d43758788f
anpospisil <anpospisil89@gmail.com>
2024-01-01 00:36:20 +0100

    updated image gallery

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -2,7 +2,7 @@ import { Box, useMediaQuery } from "@chakra-ui/react";
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 import { Picture } from "../../components/Carousel";
-
+import { ImageList } from "../../components/ImageList";
 import { useRef, useEffect } from "react";
 import "../../App.css"
 
@@ -21,12 +21,15 @@ function handleForwardClick() {
 const [isSmallerThan991] = useMediaQuery('(max-width: 991px)')
 
   return (
-    <Box>    
-      <Box className="galleryContainer"  ref={scrollRef} h={"100%"} mt={10}>
-      <Picture />
-      </Box>
-      {/* <ArrowLeftIcon onClick={handleBackClick}/>
-      <ArrowRightIcon onClick={handleForwardClick}></ArrowRightIcon> */}
-      </Box>
+    // <Box>    
+    //   <Box className="galleryContainer"  ref={scrollRef} h={"100%"} mt={10}>
+    //   <Picture />
+    //   </Box>
+    //   {/* <ArrowLeftIcon onClick={handleBackClick}/>
+    //   <ArrowRightIcon onClick={handleForwardClick}></ArrowRightIcon> */}
+    //   </Box>
+    <Box>
+      <ImageList />
+    </Box>
   );
   };
