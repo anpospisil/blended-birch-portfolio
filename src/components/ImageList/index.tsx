97b4f10980339872f3bda16c34c8896de56d1d7b
anpospisil <anpospisil89@gmail.com>
2024-01-15 00:16:14 +0100

    added images to gallery

--- src/components/ImageList/index.tsx
+++ src/components/ImageList/index.tsx
@@ -1,72 +1,88 @@
 import React, { useState, useCallback } from "react";
-import { render } from "react-dom";
+import { Box } from "@chakra-ui/react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 
 const photos:any = [
-    {
-      id: 1,
-      title: "string",  
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        id: 2,
-        title: "string",
-        src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-        width: 1,
-        height: 1
-      },
-      {
+  {
+    id: 1,
+    title: "Ayden",
+    src: "https://static.wixstatic.com/media/e65e18_14d59cd2ffc04a77b1cce88a4b4e44d3~mv2.png",
+    width: 4,
+    height: 5
+  },      {
         id: 3,
-        title: "string",
-        src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-        width: 3,
-        height: 4
+        title: "Jolieke",
+        src: "https://static.wixstatic.com/media/e65e18_b36801be567b4a20a11ef6ebb28050af~mv2.png",
+        width: 4,
+        height: 5
       },
-      {
-        id: 4,
-        title: "string",
-        src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-        width: 3,
-        height: 4
-      },
+     
       {
         id: 5,
-        title: "string",
-        src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-        width: 3,
-        height: 4
+        title: "Stephanie",
+        src: "https://static.wixstatic.com/media/e65e18_7a2a173f75714b3daa5db463ab260696~mv2.png",
+        width: 4,
+        height: 5
       },
       {
         id: 6,
-        title: "string",
-        src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+        title: "Jeff",
+        src: "https://static.wixstatic.com/media/e65e18_80900f0a43c44e3e8fed3e66931e2da5~mv2.png",
         width: 4,
-        height: 3
+        height: 5
       },
-      {
-        id: 7,
-        title: "string",
-        src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-        width: 3,
-        height: 4
-      },
+    
       {
         id: 8,
-        title: "string",
-        src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+        title: "Andrew",
+        src: "https://static.wixstatic.com/media/e65e18_03de651c371341729bc2dff4ebba0631~mv2.png",
         width: 4,
-        height: 3
+        height: 5
       },
       {
         id: 9,
-        title: "string",
-        src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+        title: "Bastian",
+        src: "https://static.wixstatic.com/media/e65e18_9db58c204dd94b47a19c07613a69b9f8~mv2.png",
         width: 4,
-        height: 3
-      }
+        height: 5
+      },
+      {
+        id: 10,
+        title: "Alice",
+        src: "https://static.wixstatic.com/media/e65e18_ecc00d734b9541b7a2ff178140e57b88~mv2.png",
+        width: 4,
+        height: 5
+      },
+      {
+        id: 4,
+        title: "Raven",
+        src: "https://static.wixstatic.com/media/e65e18_d0e72dd01d054649b0001c7c4504d3c2~mv2.png",
+        width: 100,
+        height: 73
+      },
+      {
+        id: 7,
+        title: "Dami",
+        src: "https://static.wixstatic.com/media/e65e18_9aedffbed21c41dfbb4f13cd8f1e827c~mv2.png",
+        width: 1,
+        height: 1
+      }, 
+     
+        {
+          id: 2,
+          title: "Kaarija",
+          src: "https://static.wixstatic.com/media/e65e18_b3f650815e014901afe2218ec936de16~mv2.png",
+          width: 1,
+          height: 1
+        },
+        {
+          id: 11,
+          title: "Yoofin",  
+          src: "https://static.wixstatic.com/media/e65e18_3effa6eb73e341828f5af94cb7ce758a~mv2.png",
+            width: 1,
+            height: 1
+          },
     ];
 
     export const ImageList = () => {
@@ -84,8 +100,8 @@ const photos:any = [
   };
 
   return (
-    <div>
-      <Gallery photos={photos} onClick={openLightbox} />
+    <Box >
+      <Gallery photos={photos} onClick={openLightbox} direction={"column"}/>
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
@@ -100,6 +116,6 @@ const photos:any = [
           </Modal>
         ) : null}
       </ModalGateway>
-    </div>
+    </Box>
   );
 }
--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,33 +1,18 @@
 import { Box, useMediaQuery } from "@chakra-ui/react";
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
-import { Picture } from "../../components/Carousel";
 import { ImageList } from "../../components/ImageList";
-import { useRef, useEffect } from "react";
+
 import "../../App.css"
 
-// import { useHorizontalScroll, backToStart, forwardToEnd } from "../../components/Carousel/scroll";
+
 
 
 export const Portfolio = () => {
-//   const scrollRef:any = useRef();
-//   useHorizontalScroll(scrollRef)
-//   function handleBackClick() {
-//     backToStart(scrollRef)
-// }
-// function handleForwardClick() {
-//   forwardToEnd(scrollRef)
-// }
+
 const [isSmallerThan991] = useMediaQuery('(max-width: 991px)')
 
   return (
-    // <Box>    
-    //   <Box className="galleryContainer"  ref={scrollRef} h={"100%"} mt={10}>
-    //   <Picture />
-    //   </Box>
-    //   {/* <ArrowLeftIcon onClick={handleBackClick}/>
-    //   <ArrowRightIcon onClick={handleForwardClick}></ArrowRightIcon> */}
-    //   </Box>
+
     <Box 
     maxW={"1400px"}
     px={[4, 4, 4, 14]}
